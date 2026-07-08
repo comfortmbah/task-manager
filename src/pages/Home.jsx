437bc7ea43de43a3eefcb27a95fb153200b8b068
sylvester <sylvestermbah90@gmail.com>
2026-07-08 02:00:55 -0700

    Initialize useState hook with localStorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
 import { useState, useEffect } from "react"
 
 const Home = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  })
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
+  
   return (
     <div>Home</div>
   )
