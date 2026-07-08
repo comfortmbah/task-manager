29af77b5805f59556b9bf389e7f86aff9e989b60
sylvester <sylvestermbah90@gmail.com>
2026-07-08 00:59:33 -0700

    Define application routes with root layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
+import ActiveTasks from './pages/ActiveTasks'
+import CompletedTasks from './pages/CompletedTasks'
+import NotFound from './pages/NotFound'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='active' element={<ActiveTasks />} />
+      <Route path='completed' element={<CompletedTasks />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  )
+)
 
 const App = () => {
   return (
-    <div className="text-center text-red-600 text-7xl">App</div>
+    <RouterProvider router={router} />
   )
 };
 
