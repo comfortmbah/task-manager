4443ca90b5912ca6947f23fe9ba5d307d0f7eb2f
sylvester <sylvestermbah90@gmail.com>
2026-07-28 05:02:59 -0700

    Replace useOutletContext with TodoContext

--- src/pages/ActiveTasks.jsx
+++ src/pages/ActiveTasks.jsx
@@ -1,4 +1,4 @@
-import { useOutletContext } from "react-router-dom"
+import { useTodo } from "../context/TodoContext"
 import TodoList from "../components/TodoList"
 import EmptyState from "../components/EmptyState"
 import { useEffect } from "react"
@@ -8,7 +8,7 @@ const ActiveTasks = () => {
     todos,
     handleDelete,
     handleToggle,
-  } = useOutletContext();
+  } = useTodo();
 
   const activeTasks = todos.filter((todo) => !todo.completed);
 
