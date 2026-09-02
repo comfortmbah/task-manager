5d3344e16dcb990bf9df8f8cd3cf0b247c1fb23e
sylvester <sylvestermbah90@gmail.com>
2026-09-02 05:22:21 -0700

    Add controller for creating todos

--- server/src/controllers/todoController.js
+++ server/src/controllers/todoController.js
@@ -4,3 +4,25 @@ export const getTodosList = (req, res) => {
   const todos = getTodos();
   res.json(todos);
 }
+
+export const createTodo = (req, res) => {
+  const { text } = req.body;
+
+  const todos = getTodos();
+
+  if (!text || !text.trim()) {
+    return res.status(400).json({
+      message: "Task text is required"
+    });
+  }
+
+  const newTodo = {
+    id: Date.now(),
+    text: text.trim(),
+    completed: false,
+  };
+
+  todos.push(newTodo);
+
+  res.status(201).json(newTodo);
+}
