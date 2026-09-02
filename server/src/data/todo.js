c9648f2a5fd2dc91a0c2577fcf391507827619df
sylvester <sylvestermbah90@gmail.com>
2026-09-02 04:53:31 -0700

    Add temporary todo data store

---
+++ server/src/data/todo.js
@@ -0,0 +1,21 @@
+const todos = [
+  {
+    id: 1,
+    text: "Learn Express",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "Build a REST API",
+    completed: false,
+  },
+  {
+    id: 3,
+    text: "Connect React to Express",
+    completed: TextTrackCue,
+  },
+];
+
+export const getTodos = () => {
+  return todos;
+}
