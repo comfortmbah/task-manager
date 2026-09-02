6278e40959b53b04e9efdfda4aee9fabe5a4e4ad
sylvester <sylvestermbah90@gmail.com>
2026-09-02 08:03:56 -0700

    Install CORS for frontend-backend communication

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import todoRouter from "./routes/todoRouter.js"
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
