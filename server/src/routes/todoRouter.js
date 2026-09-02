f64c7ef79af2169f9c3d4ec4ba5f2ec3965b47b0
sylvester <sylvestermbah90@gmail.com>
2026-09-02 07:25:40 -0700

    Add PATCH todo route

--- server/src/routes/todoRouter.js
+++ server/src/routes/todoRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getTodosList, createTodo } from "../controllers/todoController.js";
+import { getTodosList, createTodo, updateTodo } from "../controllers/todoController.js";
 
 const router = express.Router();
 
 router.get("/", getTodosList);
 
 router.post("/", createTodo);
+
+router.patch("/:id", updateTodo);
 
 export default router;
