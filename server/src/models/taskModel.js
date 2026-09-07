2ec555bf4e2cbb5b58f817273ff7f0d6be774a37
sylvester <sylvestermbah90@gmail.com>
2026-09-07 08:12:08 -0700

    Move models to src directory

--- server/src/models/taskModel.js
+++ server/src/models/taskModel.js
@@ -1,4 +1,4 @@
-import pool from "../config/db.js";
+import pool from "../../config/db.js";
 
 export const getAllTasks = async () => {
   const result = await pool.query("SELECT * FROM tasks ORDER BY id ASC")
