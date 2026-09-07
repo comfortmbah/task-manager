c17bb1a6ab4d56ceae482bd682c3b59362cd63e2
sylvester <sylvestermbah90@gmail.com>
2026-09-07 04:23:12 -0700

    Rename the API URL

--- client/src/api/todosApi.js
+++ client/src/api/todosApi.js
@@ -1,5 +1,5 @@
 import { handleApiResponse } from "./apiError";
-export const API_URL = "http://localhost:5000/api/todos";
+export const API_URL = "http://localhost:5000/api/tasks";
 
 
 export async function getTodos() {
