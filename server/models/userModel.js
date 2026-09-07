e398a73c06ff74b55c471dfccc95cc2e152e4418
sylvester <sylvestermbah90@gmail.com>
2026-09-07 06:44:23 -0700

    Add create user database function

---
+++ server/models/userModel.js
@@ -0,0 +1,12 @@
+import pool from "../config/db.js";
+
+export const createUser = async (name, email) => {
+  const result = await pool.query(
+    `INSERT INTO users (name, email)
+    VALUES ($1, $2)
+    RETURNING *`,
+    [name, email]
+  );
+
+  return result.rows[0];
+}
