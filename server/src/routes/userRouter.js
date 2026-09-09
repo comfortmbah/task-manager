a142d4e537d16f293b1e2bec5a712478d01f9e13
sylvester <sylvestermbah90@gmail.com>
2026-09-09 08:21:11 -0700

    Add protected user route

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { createUserController, loginUserController } from "../controllers/userController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", createUserController);
 
 router.post("/login", loginUserController);
+
+router.get("/me", authenticate, (req, res) => {
+  res.json({
+    message: "You are authenticated",
+    user: req.user
+  });
+});
 
 export default router;
