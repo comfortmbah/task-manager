9a1ed636339f9d5c122d360b886a8af9953f424a
sylvester <sylvestermbah90@gmail.com>
2026-09-09 08:47:37 -0700

    Protect task routes

--- server/src/routes/taskRouter.js
+++ server/src/routes/taskRouter.js
@@ -1,16 +1,17 @@
 import express from "express";
+import { authenticate } from "../middleware/authMiddleware.js";
 import { getTasks, createTaskController, updateTaskController, deleteTaskController, deleteCompletedTasksController } from "../controllers/taskController.js";
 
 const router = express.Router();
 
-router.get("/", getTasks);
+router.get("/", authenticate, getTasks);
 
-router.post("/", createTaskController);
+router.post("/", authenticate, createTaskController);
 
-router.patch("/:id", updateTaskController);
+router.patch("/:id", authenticate,  updateTaskController);
 
-router.delete("/completed", deleteCompletedTasksController);
+router.delete("/completed", authenticate, deleteCompletedTasksController);
 
-router.delete("/:id", deleteTaskController);
+router.delete("/:id", authenticate, deleteTaskController);
 
 export default router;
