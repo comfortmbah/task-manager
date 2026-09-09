1b25e435b0cd9693daf0ae2477245e1961a2a63b
sylvester <sylvestermbah90@gmail.com>
2026-09-09 10:58:10 -0700

    Add frontend login api

---
+++ client/src/api/userApi.js
@@ -0,0 +1,22 @@
+const API_URL = "http://localhost:5000/api/users";
+
+export async function loginUser(email, password) {
+  const response = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message || "Login failed");
+  }
+
+  const data = await response.json();
+
+  localStorage.setItem("token", data.token);
+
+  return data;
+}
