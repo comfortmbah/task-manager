075205621628de9c92fdeb0144843138f989d2df
sylvester <sylvestermbah90@gmail.com>
2026-09-10 11:35:31 -0700

    Hide welcome message when logged out

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -69,7 +69,9 @@ const Navbar = () => {
                 </li> 
 
                 <li>
-                  <span className="text-sm font-medium text-gray-700">Welcome, {user?.name}</span> {' '}
+                  {user && (
+                    <span className="text-sm font-medium text-gray-700">Welcome, {user?.name}</span> 
+                  )} {' '}
                   <button
                   onClick={handleLogout}
                   className="rounded-md border border-red-500 px-4 py-2 text-sm font-medium text-red-500 transition
