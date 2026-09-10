03de66d5a269c176b4148e7f3ef99c0c2669a72b
sylvester <sylvestermbah90@gmail.com>
2026-09-10 11:27:31 -0700

    Display authenticated user name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 
 const Navbar = () => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
   const navigate = useNavigate();
 
   const linkStyles = "rounded-md px-4 py-2 text-sm font-medium transition-colors duration-300";
@@ -69,13 +69,14 @@ const Navbar = () => {
                 </li> 
 
                 <li>
+                  <span className="text-sm font-medium text-gray-700">Welcome, {user?.name}</span> {' '}
                   <button
                   onClick={handleLogout}
                   className="rounded-md border border-red-500 px-4 py-2 text-sm font-medium text-red-500 transition
                     hover:bg-red-500 hover:text-white"
                   >
-                    Logout
-                  </button>
+                    Logout 
+                  </button> 
                 </li>
             </ul>
         </nav>
