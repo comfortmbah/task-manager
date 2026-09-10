8e703e965e01e7a00cd822a59b8941c2912587b7
sylvester <sylvestermbah90@gmail.com>
2026-09-10 10:58:23 -0700

    Add find user by id

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -23,3 +23,14 @@ export const findUserByEmail = async (email) => {
 
   return result.rows[0];
 }
+
+export const findUserById = async (id) => {
+  const result = await pool.query(
+    `SELECT id, name, email
+    FROM users
+    WHERE id = $1`,
+    [id]
+  );
+
+  return result.rows[0];
+}
