d193214caa46e012f3accd1bbbea630cf8f6ed81
sylvester <sylvestermbah90@gmail.com>
2026-09-10 08:31:17 -0700

    Connect auth provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { TodoProvider } from './context/TodoContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TodoProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </TodoProvider>
   </StrictMode>,
 )
