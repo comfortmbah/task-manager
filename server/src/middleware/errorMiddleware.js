a8e15bec70cc361b1a8bd210b418226000623537
sylvester <sylvestermbah90@gmail.com>
2026-09-13 03:44:18 -0700

    Handle duplicate user email

--- server/src/middleware/errorMiddleware.js
+++ server/src/middleware/errorMiddleware.js
@@ -1,6 +1,12 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
+  if (err.code === "23505") {
+    return res.status(409).json({
+      message: "A user with this email already exist",
+    });
+  }
+
   res.status(500).json({
     message: "Internal server error"
   });
