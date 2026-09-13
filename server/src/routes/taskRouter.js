e53cf4311046ca6bad92343a50d532e6bd33663a
sylvester <sylvestermbah90@gmail.com>
2026-09-13 05:20:28 -0700

    Wrap task routes with asyncHandler

--- server/src/routes/taskRouter.js
+++ server/src/routes/taskRouter.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import { getTasks, createTaskController, updateTaskController, deleteTaskController, deleteCompletedTasksController } from "../controllers/taskController.js";
 
 const router = express.Router();
 
-router.get("/", authenticate, getTasks);
+router.get("/", authenticate, asyncHandler(getTasks));
 
-router.post("/", authenticate, createTaskController);
+router.post("/", authenticate, asyncHandler(createTaskController));
 
-router.patch("/:id", authenticate,  updateTaskController);
+router.patch("/:id", authenticate, asyncHandler(updateTaskController));
 
-router.delete("/completed", authenticate, deleteCompletedTasksController);
+router.delete("/completed", authenticate, asyncHandler(deleteCompletedTasksController));
 
-router.delete("/:id", authenticate, deleteTaskController);
+router.delete("/:id", authenticate, asyncHandler(deleteTaskController));
 
 export default router;
