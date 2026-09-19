c16f8c7d9dd51a4eb4a925208219514856db5d4c
sylvester <sylvestermbah90@gmail.com>
2026-09-19 09:51:51 -0700

    Prevent blank task text in database

---
+++ server/migrations/1789835750608_prevent-empty-task-text.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addConstraint("tasks", "tasks_text_not_blank", {
+    check: "length(trim(text)) > 0",
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropConstraint("tasks", "tasks_text_not_blank");
+};
