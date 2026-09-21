cfd9c24f26b2a8d5e5d98488c4d5deabb71d2ccb
sylvester <sylvestermbah90@gmail.com>
2026-09-21 15:18:16 -0700

    Prevent blank user names in the database

---
+++ server/migrations/1790028173462_prevent-blank-user-name.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addConstraint("users", "users_name_not_blank", {
+    check: "length(trim(name)) > 0",
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropConstraint("users", "users_name_not_blank");
+};
