1bf3473789bde906e2721b6599f72746b5d29df9
sylvester <sylvestermbah90@gmail.com>
2026-09-23 03:41:31 -0700

    Add task text search index

---
+++ server/migrations/1790159705885_add-task-search-index.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    CREATE INDEX 
+    idx_tasks_text_trgm
+    ON tasks
+    USING GIN (text gin_trgm_ops);
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`
+    DROP INDEX IF EXISTS
+    idx_tasks_text_trgm;
+  `);
+};
