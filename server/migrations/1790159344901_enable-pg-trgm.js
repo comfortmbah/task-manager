152880bd1ad4f3e5c0b5fc04a264e90f14ff3314
sylvester <sylvestermbah90@gmail.com>
2026-09-23 03:33:36 -0700

    Enable pg_trgm extension

---
+++ server/migrations/1790159344901_enable-pg-trgm.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    CREATE EXTENSION IF NOT EXISTS
+    pg_trgm;
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`
+    DROP EXTENSION IF EXISTS
+    pg_trgm;
+  `);
+};
