348a86e0795bdfb79c176cb40f9c04512741cf11
sylvester <sylvestermbah90@gmail.com>
2026-09-24 10:10:17 -0700

    Test successful user login

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,51 +1,27 @@
-import { describe, it, expect, afterEach } from 'vitest';
+import { describe, it, expect, afterEach, beforeEach } from 'vitest';
 import request from "supertest";
 import app from "../src/app.js";
 import pool from "../config/db.js";
+import { createUser } from '../src/models/userModel.js';
 
-afterEach(async () => {
-  await pool.query(
-    "DELETE FROM users WHERE email = $1",
-    ["testuser@example.com"]
-  );
-});
-
-describe("POST /api/users", () => {
-  it("should return 400 when required fields are missing", async () => {
-    const response = await request(app)
-    .post("/api/users")
-    .send({});
-
-    expect(response.status).toBe(400);
-
-    expect(response.body).toEqual({
-      message: "Name, email and password are required",
-    });
-  });
-
-  it("should register a new user successfully", async () => {
-    const response = await request(app)
-    .post("/api/users")
-    .send({
-      name: "Test user",
-      email: "testuser@example.com",
-      password: "password123"
-    });
-
-    expect(response.status).toBe(201);
-
-    expect(response.body.user).toMatchObject({
-      name: "Test user",
-      email: "testuser@example.com",
-    });
-
-    expect(response.body.user).not.toHaveProperty("password_hash");
-
-    expect(response.body).toHaveProperty("token");
-  });
-});
+   
 
 describe("POST /api/users/login", () => {
+  beforeEach(async () => {
+    await createUser(
+      "Test User",
+      "testuser@example.com",
+      "password123"
+    );
+  });
+
+  afterEach(async () => {
+    await pool.query(
+      "DELETE FROM users WHERE email = $1",
+      ["testuser@example.com"]
+    );
+  });
+
   it("should return 400 when email and password are missing", async () => {
     const response = await request(app)
     .post("/api/users/login")
@@ -56,5 +32,18 @@ describe("POST /api/users/login", () => {
     expect(response.body).toEqual({
       message: "Email and password are required",
     });
+  });
+
+  it("should login successfully with valid credentials", async () => {
+    const response = await request(app)
+    .post("/api/users/login")
+    .send({
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty("token");
   });
 });
